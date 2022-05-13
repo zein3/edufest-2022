d90870b4781eeaf4bdc9f06089106f7ef27aa362
zein3 <zeinhaddad02@gmail.com>
2022-05-13 10:02:32 +0700

    tambahin animasi

--- js/anim.js
+++ js/anim.js
@@ -7,3 +7,12 @@ anime({
     autoplay: true
 });
 
+anime({
+    targets: '.frosted-glass',
+    opacity: 1,
+    translateY: '-0.5rem',
+    duration: 1000,
+    delay: 100,
+    endDelay: 100,
+    easing: 'easeInOutSine'
+})
